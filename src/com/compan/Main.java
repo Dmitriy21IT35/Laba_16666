ed65f20e27b41f13782a291063199c8172f0211b
Dmitriy21IT35 <97725909+Dmitriy21IT35@users.noreply.github.com>
2022-06-03 17:51:23 +0300

    Update Main.java

--- src/com/compan/Main.java
+++ src/com/compan/Main.java
@@ -1,21 +1,16 @@
 package com.compan;
 
 public class Main {
+
     public static class P extends F {
-        private static double a,b,c;
+        private static double a, b, c;
 
         public P(double a, double b, double c) {
-            super ();
             P.a = a;
             P.b = b;
             P.c = c;
         }
 
-        public static double display1() {
-            double s =  a*Math.pow (b, c);
-            System.out.println (s);
-            return s;
-        }
     }
 
     public static class F {
@@ -32,39 +27,29 @@ public class Main {
         }
 
         static double display2() {
-             double s = a*Math.pow (b, c);
+            double s = a * Math.pow (b, c);
             System.out.println (s);
-             return s;
-         }
+            return s;
+        }
     }
 
     public static void main(String[] args) {
 
-        double b;
-        double c;
-        double a;
-        P p = new P(34, 120, -3);
 
-        Converter<P, F> converter11 = new Converter<P, F> () {
-            @Override
-            public F convert(P x) {
-                return new F (P.a, P.b, P.c);
-            }
-
-            @Override
-            public <T> boolean isNotNull(T t) {
-                return false;
-            }
-        };
+        P p = new P (5, 7, 3);
+        Converter<P, F> converter11 = x -> new F(P.a, P.b, P.c);
 
         F f = converter11.convert(p);
-        System.out.println("объект прошедший конвертацию: " + F.display2());
+        System.out.println("Result from convert: " + F.display2());
+
     }
-}
+
     interface Converter<T, N> {
 
         N convert(T t);
 
-         <T> boolean isNotNull(T t);
+        static <T> boolean isNotNull(T t) {
+            return t != null;
+        }
     }
-
+}
